146d393539e6e92996fd4cbd0e46875ee3dcd718
Liam Goodacre <goodacre.liam@gmail.com>
2024-11-11 20:03:12 +0000

    fixup nvm

--- provision.sh
+++ provision.sh
@@ -145,6 +145,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
   NVM_DIR="$HOME/.config/nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  nvm install node latest
   nvm use node latest
 )
 
