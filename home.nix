d31b1a46e8ae12dd9d03e5fb1ee63aea709cd3db
Liam Goodacre <goodacre.liam@gmail.com>
2024-12-31 11:35:38 +0000

    home flake nix

--- home.nix
+++ home.nix
@@ -16,6 +16,11 @@
   # release notes.
   home.stateVersion = "24.11"; # Please read the comment before changing.
 
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
@@ -75,6 +80,13 @@
     # EDITOR = "nvim";
   };
 
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    nix-direnv.enable = true;
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
 }
